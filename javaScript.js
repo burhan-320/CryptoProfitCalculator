0936089d0b92822923787f82186879cc14e72048
bur320 <136945431+bur320@users.noreply.github.com>
2024-01-08 12:42:54 +0500

    update api

--- javaScript.js
+++ javaScript.js
@@ -52,8 +52,7 @@ cryptoCurrency.addEventListener("change", function () {
 async function getPrice(cryptoCurrency) {
   try {
     const res = await fetch(
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=250&page=1&sparkline=false`
-      // ` https://api.coingecko.com/api/v3/simple/price?ids=${cryptoCurrency}&vs_currencies=usd`
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`
     );
     const data = await res.json();
 
@@ -138,7 +137,6 @@ calculate.addEventListener("click", function (e) {
   //   fade inn results
   profit.classList.add("fadeIn")(
     //   clear filed
-
     function () {
       amount.value = "";
       price.value = "";
